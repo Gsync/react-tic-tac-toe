e6e05f9260b53e0992c9e52b099db03db31df596
Khuram <knshaikh@gmail.com>
2017-06-20 20:58:43 -0600

    add click event for player symbol

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/main.css';
 
+var p1symb = 'X';
+var p2symb = 'O';
+
+var currentTurn = p1symb;
+
+var board = document.querySelector('.board');
+board.addEventListener('click', function(e) {
+    e.target.innerHTML = currentTurn;
+    currentTurn = currentTurn === p1symb ? p2symb : p1symb;
+});
